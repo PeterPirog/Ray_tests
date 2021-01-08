336357c16da1cdc7bf0f19bd5ff7a143f6480794
PeterPirog <peterpirogtf@gmail.com>
2021-01-08 14:06:02 +0100

    initial commit

--- run_agent.py
+++ run_agent.py
@@ -1,35 +1,59 @@
 
 import gym
+import ray
+from ray import tune
 import ray.rllib.agents.ppo as ppo
+from ray.tune.schedulers import ASHAScheduler
 
 #checkpoint_path='/home/peterpirog/PycharmProjects/Ray_tests/ray_results/PPO/PPO_CartPole-v0_f82cf_00002_2_lr=0.0001_2021-01-07_15-48-11/checkpoint_5'
 
 
-import ray
-from ray import tune
 
+
+
+#### STEP 1 - CONFIGURE CLUSTER HEAD
+# DOCS IN LINK: https://docs.ray.io/en/master/package-ref.html
 ray.init(num_cpus=8,
          num_gpus=1,
          include_dashboard=True,
          dashboard_host='0.0.0.0')
 
 
+#### STEP 2 - CONFIGURE TRAINER CONFIG DICTIONARY
+# DOCS IN LINK: https://docs.ray.io/en/master/rllib-training.html#evaluating-trained-policies
+# DOCS ABOUT SEARCHING OPTIONS HERE: https://docs.ray.io/en/master/tune/api_docs/search_space.html
 config={
-        "env": "CartPole-v0",
+        "env": "CartPole-v0",   #name of environment from gym library, it can be defined by user, example:
         "num_gpus": 0,
-        "num_workers": 1,
+        "num_workers": 2, #every trainer has theese number of workers
         "lr": tune.grid_search([0.01, 0.001, 0.0001]),
-        "framework":"tf2",
+        "framework":"tf2",  #configuration to use tensorflow 2 as a main framework
     }
+#### STEP 3 - CONFIGURE TUNE SCHEDULER
+# DOCS IN LINK: https://docs.ray.io/en/master/tune/api_docs/schedulers.html?highlight=ASHAScheduler#asha-tune-schedulers-ashascheduler
+
+asha_scheduler = ASHAScheduler(
+    time_attr='training_iteration', #use number of iterations as limiter
+    metric='episode_reward_mean',   #parameter to optimize
+    #mode='max',                     #find maximum
+    max_t=10,                        # maximum number of iterations -  stop trainng after max_t iterations
+    grace_period=2,                 # stop after grace_period iterations without result improvement
+    reduction_factor=3,
+    brackets=1)
 
 
-
+#### STEP 3 - CONFIGURE RAY TUNE
+# DOCS IN LINK: https://docs.ray.io/en/master/tune/api_docs/execution.html#tune-run
 
 analysis=tune.run(
-    "PPO",
-    stop={"episode_reward_mean": 10},
-    mode='max',
-    checkpoint_at_end=True,
+    run_or_experiment="PPO",
+    scheduler=asha_scheduler,
+    keep_checkpoints_num = 3,
+    checkpoint_freq = 3,
+    checkpoint_at_end = True,
+    stop={"episode_reward_mean": 100}, #stop training if this value is reached
+    mode='max',  # Do not use here if you defined mode before in scheduler
+    reuse_actors = True,
     config=config,
 )
 
