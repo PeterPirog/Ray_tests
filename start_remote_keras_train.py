9287a1b89f09c69c8150b6162610c45e5840f11f
PeterPirog <peterpirogtf@gmail.com>
2021-01-16 15:00:41 +0100

    backup files created

--- start_remote_keras_train.py
+++ start_remote_keras_train.py
@@ -5,31 +5,33 @@ file='tune_keras_functional.py' #run_PPO_hard_walker.py python3  run_PPO_hard_wa
 
 
 #Set2
-local_result_dir='~/ray_results/' # path in physical machine /home/peterpirog/PycharmProjects/Ray_tests/ray_results/
+local_result_dir='/home/peterpirog/ray_results/' # path in physical machine /home/peterpirog/PycharmProjects/Ray_tests/ray_results/
 docker_result_dir='/root/ray_results/'  #path in docker container
 
-#docker_cmd=f'sudo docker run -p 8265:8265 -p 6379:6379 -v `pwd`:`pwd` -v {local_result_dir}:{docker_result_dir} -w `pwd` --gpus all  --cpus 8 -it --rm --shm-size=16g peterpirogtf/ray_tf2:latest'
 docker_cmd=f'sudo docker run' \
+           f' -it --rm' \
+           f'  -v `pwd`:`pwd`' \
+           f' -v {local_result_dir}:{docker_result_dir}'\
+           f' -w `pwd`' \
+           f' --network=host' \
            f' -p 8265:8265' \
            f' -p 6379:6379' \
            f' --expose=10000-10999' \
-           f'  -v `pwd`:`pwd`' \
-           f' -v {local_result_dir}:{docker_result_dir}' \
-           f' -w `pwd`' \
            f' --gpus all' \
            f'  --cpus 8' \
-           f' -it --rm' \
            f' --shm-size=16g' \
-           f' --network=host' \
            f' peterpirogtf/ray_tf2:latest'
 
 rllib_cmd=f'python3 {file}'
 
 cmd=docker_cmd + ' ' + rllib_cmd
 
-
-
+try:
+    os.system('tensorboard --logdir ~/ray_results --bind_all --port 6006')
+except:
+    pass
 os.system(cmd)
+
 
 """
 If You want use docker, you have to install docker application at first !!!
@@ -44,10 +46,11 @@ comments for docker run options (full description here https://docs.docker.com/e
 -d - container works in background (typically container is removed after using) you can log in inside container by command:
     "docker ps"
     which returns CONTAINER_ID, copy it and use command
-    "docker attach ijhweifhb0wqnih768327483y8erfhnnkfjnkfnk"   <- use id of your container, not this !!!
+    "docker attach c92c2f29718e"   <- use id of your container, not this !!!
 
     Directories
-
+-v `pwd`:`pwd` - mount a volume, current directory of your machine with docker root directory,  using of ` sign is important not " nor '
+-w - set working directory inside the container
     Network:
 --network=host - option to connect container with physical machine network, without this option container use internal interface with address 127.0.0.1, while errors check if ports are busy    
 -p 8265:8265 - option for expose dashboard port
@@ -68,4 +71,8 @@ IMPORTANT: You can't use asset in script, if You don't declare it for Your docke
             
 --cpus 8 - number of CPU available for docker. If You declare 4, ray can use only 4 CPU even in physical machine are 8
 --shm-size=16g' - it means get access to 16 GB of RAM
+
+If you want use tensorboard command:
+tensorboard --logdir ~/ray_results --bind_all --port 6006'
+
 """
--- tune_keras_functional.py
+++ tune_keras_functional.py
@@ -1,6 +1,6 @@
-
 from tensorflow.keras.datasets import mnist
 from ray.tune.integration.keras import TuneReportCallback
+
 
 def train_mnist(config):
     # https://github.com/tensorflow/tensorflow/issues/32159
@@ -13,16 +13,16 @@ def train_mnist(config):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    #define model
-    inputs=tf.keras.layers.Input(shape=(28, 28))
-    x=tf.keras.layers.Flatten()(inputs)
-    #x=tf.keras.layers.LayerNormalization()(x)
+    # define model
+    inputs = tf.keras.layers.Input(shape=(28, 28))
+    x = tf.keras.layers.Flatten()(inputs)
+    # x=tf.keras.layers.LayerNormalization()(x)
     for i in range(config["layers"]):
-        x=tf.keras.layers.Dense(units=config["hidden"], activation=config["activation"])(x)
-        x=tf.keras.layers.Dropout(config["dropout"])(x)
-    outputs=tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
+        x = tf.keras.layers.Dense(units=config["hidden"], activation=config["activation"])(x)
+        x = tf.keras.layers.Dropout(config["dropout"])(x)
+    outputs = tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
 
-    model=tf.keras.Model(inputs=inputs,outputs=outputs,name="mnist_model")
+    model = tf.keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
     model.compile(
         loss="sparse_categorical_crossentropy",
         optimizer=tf.keras.optimizers.Adam(lr=config["lr"]),
@@ -50,9 +50,16 @@ if __name__ == "__main__":
 
     mnist.load_data()  # we do this on the driver because it's not threadsafe
 
-    ray.init(num_cpus=8, num_gpus=1)
-    #sched = AsyncHyperBandScheduler(time_attr="training_iteration", max_t=400, grace_period=20)
-    sched = ASHAScheduler(time_attr="training_iteration", max_t=100, grace_period=10)
+    ray.init(num_cpus=8, num_gpus=1,
+             include_dashboard=True,  # if you use docker use docker run -p 8265:8265 -p 6379:6379
+             dashboard_host='0.0.0.0')
+
+    sched = ASHAScheduler(time_attr="training_iteration",
+                          max_t=100,
+                          grace_period=10,
+                          #mode='max',                     #find maximum, do not define here if you define in tune.run
+                          reduction_factor=3,
+                          brackets=1)
 
     analysis = tune.run(
         train_mnist,
@@ -62,20 +69,20 @@ if __name__ == "__main__":
         mode="max",
         stop={
             "mean_accuracy": 0.99,
-            "training_iteration": 300
+            "training_iteration": 200
         },
-        num_samples=50, #10
-        local_dir='../ray_results',
+        num_samples=20,
+        #local_dir='/root/ray_results/',  #this is default dir inside docker
         resources_per_trial={
-            "cpu": 8,
-            "gpu": 1
+            "cpu": 1,
+            "gpu": 0
         },
         config={
-            #"threads": 2,
+            # "threads": 2,
             "lr": tune.uniform(0.001, 0.1),
             "hidden": tune.randint(32, 512),
             "dropout": tune.uniform(0.01, 0.2),
-            "activation": tune.choice(["relu","elu"]),
-            "layers": tune.choice([1,2,3])
+            "activation": tune.choice(["relu", "elu"]),
+            "layers": tune.choice([1, 2, 3])
         })
     print("Best hyperparameters found were: ", analysis.best_config)
